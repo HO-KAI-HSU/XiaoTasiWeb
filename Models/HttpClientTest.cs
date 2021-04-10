80e263aaead3d0ba717d94ad9e41fa783257b02f
george00157127@gmail.com <George157127>
2021-04-10 13:23:27 +0800

    What : 補上HttpClientTest.cs檔案

---
+++ Models/HttpClientTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace xiaotasi.Models
+{
+    public class HttpClientTest
+    {
+        public string httpClientGet(string url)
+        {
+            var client = new HttpClient();
+            //client.BaseAddress = new Uri(url);
+
+            var response = client.GetAsync(url).Result;
+            var body = response.Content.ReadAsStringAsync().Result;
+            //TODO:反序列化
+            //var serializer = new JavaScriptSerializer();
+            //string list = serializer.Deserialize<string>(body);
+            return body;
+        }
+
+        public string httpClientPost(string url, string phone)
+        {
+            var client = new HttpClient();
+            //client.BaseAddress = new Uri(url);
+            // 準備寫入的 data
+            PostData postData = new PostData() { phone = phone };
+            // 將 data 轉為 json
+            string json = JsonConvert.SerializeObject(postData);
+            // 將轉為 string 的 json 依編碼並指定 content type 存為 httpcontent
+            HttpContent contentPost = new StringContent(json, Encoding.UTF8, "application/json");
+            // 發出 post 並取得結果
+
+            HttpResponseMessage response = client.PostAsync(url, contentPost).Result;
+            var body = response.Content.ReadAsStringAsync().Result;
+            return body;
+        }
+    }
+
+    internal class PostData
+    {
+        public PostData()
+        {
+        }
+
+        public string phone { get; set; }
+    }
+}
