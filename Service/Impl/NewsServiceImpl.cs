642fe66cf76b3dd6fb4951f7aa3b839e686d3969
george00157127@gmail.com <George157127>
2022-12-15 20:08:07 +0800

    Fix latest_news show error

--- Service/Impl/NewsServiceImpl.cs
+++ Service/Impl/NewsServiceImpl.cs
@@ -23,7 +23,7 @@ namespace xiaotasi.Service.Impl
             string timeNow = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt32(DateTime.UtcNow.AddHours(8).Subtract(new DateTime(1970, 1, 1)).TotalSeconds)).DateTime.ToString("yyyy-MM-dd HH:mm:ss");
             SqlConnection connection = new SqlConnection(connectionString);
             // SQL Command
-            SqlCommand select = new SqlCommand("select latest_news_id, latest_news_title, latest_news_en_title, latest_news_content, latest_news_en_content, latest_news_url, latest_news_pic_path, f_date from latest_news_list where publish_s_time <= @time and publish_e_time >= @time", connection);
+            SqlCommand select = new SqlCommand("select latest_news_id, latest_news_title, latest_news_content, latest_news_url, latest_news_pic_path, f_date from latest_news_list where publish_s_time <= @time and publish_e_time >= @time", connection);
             select.Parameters.Add("@time", System.Data.SqlDbType.DateTime).Value = timeNow;
             // 開啟資料庫連線
             await connection.OpenAsync();
@@ -34,12 +34,10 @@ namespace xiaotasi.Service.Impl
                 NewsPojo newsData = new NewsPojo();
                 newsData.newsId = reader.GetInt32(0);
                 newsData.newsTraditionalTitle = reader.IsDBNull(1) ? "" : reader.GetSqlString(1).ToString();
-                newsData.newsEnTitle = reader.IsDBNull(2) ? "" : (string)reader[2].ToString();
-                newsData.newsTraditionalContent = reader.IsDBNull(3) ? "" : reader.GetSqlString(3).ToString();
-                newsData.newsEnContent = reader.IsDBNull(4) ? "" : (string)reader[4].ToString();
-                newsData.newsPicPath = reader.IsDBNull(6) ? "" : (string)reader[6].ToString();
+                newsData.newsTraditionalContent = reader.IsDBNull(2) ? "" : reader.GetSqlString(2).ToString();
+                newsData.newsPicPath = reader.IsDBNull(4) ? "" : (string)reader[4].ToString();
                 string format = "yyyy-MM-dd";
-                newsData.date = ((DateTime)reader[7]).ToString(format);
+                newsData.date = ((DateTime)reader[5]).ToString(format);
                 newsData.newsUrl = "";
                 newsList.Add(newsData);
             }
@@ -54,7 +52,7 @@ namespace xiaotasi.Service.Impl
             string timeNow = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt32(DateTime.UtcNow.AddHours(8).Subtract(new DateTime(1970, 1, 1)).TotalSeconds)).DateTime.ToString("yyyy-MM-dd HH:mm:ss");
             SqlConnection connection = new SqlConnection(connectionString);
             // SQL Command
-            SqlCommand select = new SqlCommand("select latest_media_news_id, latest_media_news_title, latest_media_news_en_title, latest_media_news_content, latest_media_news_en_content, latest_media_news_url, latest_media_news_pic_path, f_date from latest_media_news_list where publish_s_time <= @time and publish_e_time >= @time and latest_media_news_type = 1", connection);
+            SqlCommand select = new SqlCommand("select latest_media_news_id, latest_media_news_title, latest_media_news_content, latest_media_news_url, latest_media_news_pic_path, f_date from latest_media_news_list where publish_s_time <= @time and publish_e_time >= @time and latest_media_news_type = 1", connection);
             select.Parameters.Add("@time", System.Data.SqlDbType.DateTime).Value = timeNow;
             // 開啟資料庫連線
             await connection.OpenAsync();
@@ -65,13 +63,11 @@ namespace xiaotasi.Service.Impl
                 MediaNewsPojo newsData = new MediaNewsPojo();
                 newsData.mediaNewsId = reader.GetInt32(0);
                 newsData.mediaNewsTraditionalTitle = reader.IsDBNull(1) ? "" : (string)reader[1];
-                newsData.mediaNewsEnTitle = reader.IsDBNull(2) ? "" : (string)reader[2];
-                newsData.mediaNewsTraditionalContent = reader.IsDBNull(3) ? "" : (string)reader[3];
-                newsData.mediaNewsEnContent = reader.IsDBNull(4) ? "" : (string)reader[4];
-                newsData.mediaNewsMovieUrl = reader.IsDBNull(5) ? "" : (string)reader[5];
-                newsData.mediaNewsPicPath = reader.IsDBNull(6) ? "" : (string)reader[6];
+                newsData.mediaNewsTraditionalContent = reader.IsDBNull(2) ? "" : (string)reader[2];
+                newsData.mediaNewsMovieUrl = reader.IsDBNull(3) ? "" : (string)reader[3];
+                newsData.mediaNewsPicPath = reader.IsDBNull(4) ? "" : (string)reader[4];
                 string format = "yyyy-MM-dd";
-                newsData.date = ((DateTime)reader[7]).ToString(format);
+                newsData.date = ((DateTime)reader[5]).ToString(format);
                 newsData.mediaNewsMovieUrl = "";
                 mediaNewsList.Add(newsData);
             }
